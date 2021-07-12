5b4ea2caabca1858187393f5d188fc9b01ef62a2
eslipyj <eslipyj@gmail.com>
2021-07-12 13:26:20 -0300

    semana 1

--- 2-InstruccionIf/jsInstruccionIF-01.js
+++ 2-InstruccionIf/jsInstruccionIF-01.js
@@ -1,6 +1,21 @@
+//AUTOR:Slipyj,Emanuel
+//ENUNCIADO:Ejercicio1DeIf
 function mostrar()
 {
-	//tomo la edad  
-	alert("ok");
+	var edad;
 
-}//FIN DE LA FUNCIÓN
+	edad=document.getElementById("txtIdEdad").value;
+	//comparo numeros asi...;
+	//edad=parseInt(edad);
+	//	if(edad==15);
+	if(edad=="15");
+	{
+		alert("Niña bonita");
+	}	
+		alert("Fin de la funcion");
+}
+//15==15 :es true(numeros). si le pongo 3 "==="va a seguir siendo true
+//"15"=="15" :es true(palabras). si le pongo 3 "==="va a seguir siendo true
+//"15"==15 :es true(palabra igual a numero).si le pongo 3 "==="va a dar false	
+//15=="15":es true(numero igual a palabra).si le pongo 3 "==="va a dar false
+//existe el "!==" que significa que es distinto también.
--- 2-InstruccionIf/jsInstruccionIF-02.js
+++ 2-InstruccionIf/jsInstruccionIF-02.js
@@ -1,7 +1,17 @@
+//AUTOR:Slipyj,Emanuel
+//ANUNCIO:Ejercicio2
 function mostrar()
 {
-	//tomo la edad  
-	alert("ok");
-	
+	var edad;
+
+	edad=document.getElementById("txtIdEdad").value;
+	edad=parseInt(edad);
+	//si pongo
+	//if(edad > 17) funciona,pero no es tan estricto el rango
+	if(edad>=18);
+	{
+
+		alert("Es mayor de edad");
+	}
 
 }//FIN DE LA FUNCIÓN
